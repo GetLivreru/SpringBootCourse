64eb13c1e69d619c0a93251507b59866017aacd0
theadilet <110306539+GetLivreru@users.noreply.github.com>
2024-07-31 23:47:17 +0500

    some errors

--- Project1/src/main/java/com/example/Project1/integration/Book.java
+++ Project1/src/main/java/com/example/Project1/integration/Book.java
@@ -1,4 +1,30 @@
 package com.example.Project1.integration;
+import com.example.Project1.validators.CurrentYear;
+import lombok.*;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(exclude = {"id"})
 public class Book {
+    private int id;
+    private Integer person_id;
+
+    @NotNull(message = "Название книги не может быть пустым")
+    @Pattern(regexp = "[0-9А-ЯЁ][0-9a-zA-Zа-яА-ЯёЁ\\-\\s]+",
+            message = "Должен быть формат \"Название\" на русском языке")
+    private String title;
+
+    @NotNull(message = "Фамилия и имя автор не могут быть пустыми")
+    @Pattern(regexp = "[А-ЯЁ][а-яА-ЯёЁ\\-]+\\s[А-ЯЁ][а-яА-ЯёЁ\\-]+",
+            message = "Должен быть формат \"Фамилия Имя\" на русском языке")
+    private String author;
+
+    @Min(value = 1445, message = "Год издания должен быть больше 1445")
+    @CurrentYear
+    private int year;
 }
--- Project1/src/main/java/com/example/Project1/integration/Person.java
+++ Project1/src/main/java/com/example/Project1/integration/Person.java
@@ -1,4 +1,24 @@
 package com.example.Project1.integration;
 
+import com.example.Project1.validators.CurrentYear;
+import lombok.*;
+import org.intellij.lang.annotations.Pattern;
+import org.jetbrains.annotations.NotNull;
+
+import javax.validation.constraints.*;
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(exclude = {"id"})
 public class Person {
+    private int id;
+
+    @NotNull(message = "ФИО не может быть пустым")
+    @Pattern(regexp = "([А-ЯЁ][а-яА-ЯёЁ\\-]+\\s){2}[А-ЯЁ][а-яё]+",
+            message = "Должен быть формат \"Фамилия Имя Отчество\" на русском языке")
+    private String name;
+
+    @Min(value = 1900, message = "Год рождения должен быть больше 1900")
+    @CurrentYear
+    private int year;
 }
