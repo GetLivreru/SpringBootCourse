8288a63ec24e413846b1f574e49beb81dbe411f7
theadilet <110306539+GetLivreru@users.noreply.github.com>
2024-08-03 15:36:09 +0500

    creatting current year validator

--- Project1/src/main/java/com/example/Project1/validators/CurrentYearValidator.java
+++ Project1/src/main/java/com/example/Project1/validators/CurrentYearValidator.java
@@ -1,7 +1,13 @@
 package com.example.Project1.validators;
 
 import jakarta.validation.ConstraintValidator;
-
+import jakarta.validation.ConstraintValidatorContext;
+import java.time.YearMonth;
 public class CurrentYearValidator implements ConstraintValidator<CurrentYear, Integer> {
+        @Override
+        public boolean isValid(Integer value, ConstraintValidatorContext context) {
+            return value <= YearMonth.now().getYear();
+        }
+
 
 }
