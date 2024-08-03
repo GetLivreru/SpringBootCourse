409f72e9dad3dee660298158b43956dd05b28821
theadilet <110306539+GetLivreru@users.noreply.github.com>
2024-08-03 16:02:16 +0500

    creating new controller

--- Project1/src/main/java/com/example/Project1/controller/HomeController.java
+++ Project1/src/main/java/com/example/Project1/controller/HomeController.java
@@ -1,4 +1,13 @@
 package com.example.Project1.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+    @GetMapping("/")
+    public String homePage() {
+        return "index";
+    }
 }
