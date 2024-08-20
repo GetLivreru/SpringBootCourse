1f7e94b45d1e140b04a746a9801d96a123274f1f
theadilet <110306539+GetLivreru@users.noreply.github.com>
2024-08-21 02:49:31 +0500

    create unit test

--- Project1/src/test/java/com/example/Project1/config/TestConfig.java
+++ Project1/src/test/java/com/example/Project1/config/TestConfig.java
@@ -1,4 +1,28 @@
 package com.example.Project1.config;
 
-public class TestConfig {
+
+import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class TestConfig implements WebMvcConfigurer {
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;NON_KEYWORDS=YEAR");
+        dataSource.setUsername("root");
+        dataSource.setPassword("password123");
+        return dataSource;
+    }
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
+    }
+
+
 }
