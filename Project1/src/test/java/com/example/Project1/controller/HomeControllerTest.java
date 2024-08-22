3260c05d69457fc6e783fd2f2b5984e8fc9a1fbb
theadilet <110306539+GetLivreru@users.noreply.github.com>
2024-08-22 21:14:23 +0500

    add home controller test

--- Project1/src/test/java/com/example/Project1/controller/HomeControllerTest.java
+++ Project1/src/test/java/com/example/Project1/controller/HomeControllerTest.java
@@ -1,10 +1,33 @@
 package com.example.Project1.controller;
 
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
 
 @ExtendWith(MockitoExtension.class)
     class HomeControllerTest {
+    private final MockMvc mockMvc;
+
+    public HomeControllerTest() {
+        mockMvc = MockMvcBuilders.standaloneSetup(new HomeController()).build();
+    }
+
+    @Test
+    public void homePageTest() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpectAll(
+                        status().isOk(),
+                        forwardedUrl("index")
+                );
+    }
+
 
 }
