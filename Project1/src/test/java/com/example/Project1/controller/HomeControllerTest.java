3e1ae54f6fd14c7dfec7dfbf2082d4e41eb7863e
theadilet <110306539+GetLivreru@users.noreply.github.com>
2024-08-22 18:18:43 +0500

    add Mockito class

--- Project1/src/test/java/com/example/Project1/controller/HomeControllerTest.java
+++ Project1/src/test/java/com/example/Project1/controller/HomeControllerTest.java
@@ -1,4 +1,10 @@
 package com.example.Project1.controller;
 
-public class HomeControllerTest {
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+    class HomeControllerTest {
+
 }
