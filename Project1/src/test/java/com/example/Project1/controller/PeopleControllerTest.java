9481a20be013eecbdc3aaa07daa7282c15393f8a
theadilet <110306539+GetLivreru@users.noreply.github.com>
2024-08-22 21:17:48 +0500

    create PeopleController Mock

--- Project1/src/test/java/com/example/Project1/controller/PeopleControllerTest.java
+++ Project1/src/test/java/com/example/Project1/controller/PeopleControllerTest.java
@@ -1,4 +1,20 @@
 package com.example.Project1.controller;
 
-public class PeopleControllerTest {
+
+import com.example.Project1.dao.PersonDAO;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ExtendWith(MockitoExtension.class)
+@TestMethodOrder(MethodOrderer.class)
+    class PeopleControllerTest {
+    private MockMvc mockMvc;
+
+    @Mock
+    private PersonDAO personDAO;
+
 }
