2f452afbe3b9105f6a6c1e2119f7b93e6e9c6ed9
theadilet <110306539+GetLivreru@users.noreply.github.com>
2024-08-22 21:32:40 +0500

    add

--- Project1/src/test/java/com/example/Project1/dao/BookDAOTest.java
+++ Project1/src/test/java/com/example/Project1/dao/BookDAOTest.java
@@ -1,4 +1,20 @@
 package com.example.Project1.dao;
+import com.example.Project1.config.TestConfig;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+
+@ExtendWith(SpringExtension.class)
+@Sql({"/jdbc/drop-book.sql", "/jdbc/drop-person.sql", "/jdbc/create-person.sql",
+        "/jdbc/create-book.sql", "/jdbc/insert-people.sql", "/jdbc/insert-books.sql"})
+@ContextConfiguration(classes = TestConfig.class)
+@TestMethodOrder(MethodOrderer.Random.class)
 public class BookDAOTest {
+    private BookDAO bookDAO;
+
+
 }
