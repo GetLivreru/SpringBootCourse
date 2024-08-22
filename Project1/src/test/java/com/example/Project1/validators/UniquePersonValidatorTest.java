331a2070d9d0bdb0b8a6cbaa9ec742b4e4ca5d84
theadilet <110306539+GetLivreru@users.noreply.github.com>
2024-08-22 23:38:57 +0500

    add test by Unique Person Validator

--- Project1/src/test/java/com/example/Project1/validators/UniquePersonValidatorTest.java
+++ Project1/src/test/java/com/example/Project1/validators/UniquePersonValidatorTest.java
@@ -1,4 +1,80 @@
 package com.example.Project1.validators;
+import com.example.Project1.config.TestConfig;
+import com.example.Project1.dao.PersonDAO;
+import com.example.Project1.integration.Person;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.Errors;
 
-public class UniquePersonValidatorTest {
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@Sql({"/jdbc/drop-book.sql", "/jdbc/drop-person.sql", "/jdbc/create-person.sql",
+        "/jdbc/create-book.sql", "/jdbc/insert-people.sql", "/jdbc/insert-books.sql"})
+@WebAppConfiguration
+@ContextConfiguration(classes = TestConfig.class)
+@TestMethodOrder(MethodOrderer.Random.class)
+ class UniquePersonValidatorTest {
+    private final UniquePersonValidator validator;
+    private Errors errors;
+
+    private Person testPerson;
+
+    @Autowired
+    public UniquePersonValidatorTest(JdbcTemplate jdbcTemplate) {
+        validator = new UniquePersonValidator(new PersonDAO(jdbcTemplate));
+    }
+
+    @BeforeEach
+    public void setUp() {
+        testPerson = new Person();
+    }
+
+    @Test
+    public void supportsTest() {
+        assertTrue(validator.supports(Person.class));
+        assertFalse(validator.supports(Object.class));
+    }
+
+    @Test
+    public void personWithNotUniqueNameTest() {
+        // given
+        testPerson.setName("Второй Тестовый Читатель");
+        errors = new BeanPropertyBindingResult(testPerson, "person");
+        // when
+        validator.validate(testPerson, errors);
+        // then
+        assertTrue(errors.hasErrors());
+        assertEquals(1, errors.getErrorCount());
+    }
+
+    @Test
+    public void samePersonTest() {
+        // given
+        testPerson.setId(3);
+        testPerson.setName("Третий Тестовый Читатель");
+        errors = new BeanPropertyBindingResult(testPerson, "person");
+        // when
+        validator.validate(testPerson, errors);
+        // then
+        assertFalse(errors.hasErrors());
+    }
+
+    @Test
+    public void personWithUniqueNameTest() {
+        // given
+        testPerson.setName("Новый Тестовый Читатель");
+        errors = new BeanPropertyBindingResult(testPerson, "person");
+        // when
+        validator.validate(testPerson, errors);
+        // then
+        assertFalse(errors.hasErrors());
+    }
 }
