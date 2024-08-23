0d42d17080919ac7b14fed9017e50ac7719254d4
iD1eat <110306539+GetLivreru@users.noreply.github.com>
2024-08-24 01:57:32 +0500

    Update Person.java
    add hibernate to Person entity

--- Project2/Project2/src/main/java/com/example/Project2/integration/Person.java
+++ Project2/Project2/src/main/java/com/example/Project2/integration/Person.java
@@ -9,19 +9,31 @@ import lombok.EqualsAndHashCode;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-@Setter
+import java.util.List;
+
+@Entity
+@Table(name = "Person")
 @Getter
+@Setter
 @NoArgsConstructor
-@EqualsAndHashCode(exclude = {"id"})
+@EqualsAndHashCode(exclude = {"id", "books"})
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
 
     @NotNull(message = "ФИО не может быть пустым")
     @Pattern(regexp = "([А-ЯЁ][а-яА-ЯёЁ\\-]+\\s){2}[А-ЯЁ][а-яё]+",
             message = "Должен быть формат \"Фамилия Имя Отчество\" на русском языке")
+    @Column(name = "name")
     private String name;
 
     @Min(value = 1900, message = "Год рождения должен быть больше 1900")
     @CurrentYear
+    @Column(name = "year")
     private int year;
+
+    @OneToMany(mappedBy = "reader")
+    private List<Book> books;
 }
